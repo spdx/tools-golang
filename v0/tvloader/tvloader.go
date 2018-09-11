3e5a0322811e2c86e31b7d64f8b9f23f262e55b0
Steve Winslow <swinslow@gmail.com>
2018-09-11 13:47:57 -0400

    Added primary tvloader package file
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

---
+++ v0/tvloader/tvloader.go
@@ -0,0 +1,4 @@
+// Package tvloader is used to load and parse SPDX tag-value documents
+// into spdx-go data structures.
+// SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+package tvloader
