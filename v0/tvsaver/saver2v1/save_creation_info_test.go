5b337ccbf0f1da096cea905de9d8d8eeb8db5758
Steve Winslow <swinslow@gmail.com>
2018-10-10 08:42:39 -0400

    Added test for omitting optional fields in Creation Info section
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

--- v0/tvsaver/saver2v1/save_creation_info_test.go
+++ v0/tvsaver/saver2v1/save_creation_info_test.go
@@ -76,5 +76,78 @@ DocumentComment: this is a document comment
 	}
 }
 
-// FIXME test to confirm that optional fields are omitted
-// FIXME test to confirm that mandatory fields are NOT omitted (??)
+func TestSaver2_1OmitsOptionalFieldsIfEmpty(t *testing.T) {
+	// --- need at least one creator; do first for Persons ---
+	ci1 := &spdx.CreationInfo2_1{
+		SPDXVersion:       "SPDX-2.1",
+		DataLicense:       "CC0-1.0",
+		SPDXIdentifier:    "SPDXRef-DOCUMENT",
+		DocumentName:      "spdx-go-0.0.1.abcdef",
+		DocumentNamespace: "https://github.com/swinslow/spdx-docs/spdx-go/spdx-go-0.0.1.abcdef.whatever",
+		CreatorPersons: []string{
+			"John Doe",
+		},
+		Created: "2018-10-10T06:20:00Z",
+	}
+
+	// what we want to get, as a buffer of bytes
+	want1 := bytes.NewBufferString(`SPDXVersion: SPDX-2.1
+DataLicense: CC0-1.0
+SPDXID: SPDXRef-DOCUMENT
+DocumentName: spdx-go-0.0.1.abcdef
+DocumentNamespace: https://github.com/swinslow/spdx-docs/spdx-go/spdx-go-0.0.1.abcdef.whatever
+Creator: Person: John Doe
+Created: 2018-10-10T06:20:00Z
+
+`)
+
+	// render as buffer of bytes
+	var got1 bytes.Buffer
+	err := renderCreationInfo2_1(ci1, &got1)
+	if err != nil {
+		t.Errorf("Expected nil error, got %v", err)
+	}
+
+	// check that they match
+	c1 := bytes.Compare(want1.Bytes(), got1.Bytes())
+	if c1 != 0 {
+		t.Errorf("Expected %v, got %v", want1.String(), got1.String())
+	}
+
+	// --- need at least one creator; now switch to organization ---
+	ci2 := &spdx.CreationInfo2_1{
+		SPDXVersion:       "SPDX-2.1",
+		DataLicense:       "CC0-1.0",
+		SPDXIdentifier:    "SPDXRef-DOCUMENT",
+		DocumentName:      "spdx-go-0.0.1.abcdef",
+		DocumentNamespace: "https://github.com/swinslow/spdx-docs/spdx-go/spdx-go-0.0.1.abcdef.whatever",
+		CreatorOrganizations: []string{
+			"John Doe, Inc.",
+		},
+		Created: "2018-10-10T06:20:00Z",
+	}
+
+	// what we want to get, as a buffer of bytes
+	want2 := bytes.NewBufferString(`SPDXVersion: SPDX-2.1
+DataLicense: CC0-1.0
+SPDXID: SPDXRef-DOCUMENT
+DocumentName: spdx-go-0.0.1.abcdef
+DocumentNamespace: https://github.com/swinslow/spdx-docs/spdx-go/spdx-go-0.0.1.abcdef.whatever
+Creator: Organization: John Doe, Inc.
+Created: 2018-10-10T06:20:00Z
+
+`)
+
+	// render as buffer of bytes
+	var got2 bytes.Buffer
+	err = renderCreationInfo2_1(ci2, &got2)
+	if err != nil {
+		t.Errorf("Expected nil error, got %v", err)
+	}
+
+	// check that they match
+	c2 := bytes.Compare(want2.Bytes(), got2.Bytes())
+	if c2 != 0 {
+		t.Errorf("Expected %v, got %v", want2.String(), got2.String())
+	}
+}
