d3d155c849a22012636ec31c6dfff54a8a21b713
Steve Winslow <swinslow@gmail.com>
2018-10-10 18:54:27 -0400

    Removed textify from Creation Info non-comment sections
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

--- v0/tvsaver/saver2v1/save_creation_info.go
+++ v0/tvsaver/saver2v1/save_creation_info.go
@@ -11,37 +11,37 @@ import (
 
 func renderCreationInfo2_1(ci *spdx.CreationInfo2_1, w io.Writer) error {
 	if ci.SPDXVersion != "" {
-		fmt.Fprintf(w, "SPDXVersion: %s\n", textify(ci.SPDXVersion))
+		fmt.Fprintf(w, "SPDXVersion: %s\n", ci.SPDXVersion)
 	}
 	if ci.DataLicense != "" {
-		fmt.Fprintf(w, "DataLicense: %s\n", textify(ci.DataLicense))
+		fmt.Fprintf(w, "DataLicense: %s\n", ci.DataLicense)
 	}
 	if ci.SPDXIdentifier != "" {
-		fmt.Fprintf(w, "SPDXID: %s\n", textify(ci.SPDXIdentifier))
+		fmt.Fprintf(w, "SPDXID: %s\n", ci.SPDXIdentifier)
 	}
 	if ci.DocumentName != "" {
-		fmt.Fprintf(w, "DocumentName: %s\n", textify(ci.DocumentName))
+		fmt.Fprintf(w, "DocumentName: %s\n", ci.DocumentName)
 	}
 	if ci.DocumentNamespace != "" {
-		fmt.Fprintf(w, "DocumentNamespace: %s\n", textify(ci.DocumentNamespace))
+		fmt.Fprintf(w, "DocumentNamespace: %s\n", ci.DocumentNamespace)
 	}
 	for _, s := range ci.ExternalDocumentReferences {
-		fmt.Fprintf(w, "ExternalDocumentRef: %s\n", textify(s))
+		fmt.Fprintf(w, "ExternalDocumentRef: %s\n", s)
 	}
 	if ci.LicenseListVersion != "" {
-		fmt.Fprintf(w, "LicenseListVersion: %s\n", textify(ci.LicenseListVersion))
+		fmt.Fprintf(w, "LicenseListVersion: %s\n", ci.LicenseListVersion)
 	}
 	for _, s := range ci.CreatorPersons {
-		fmt.Fprintf(w, "Creator: Person: %s\n", textify(s))
+		fmt.Fprintf(w, "Creator: Person: %s\n", s)
 	}
 	for _, s := range ci.CreatorOrganizations {
-		fmt.Fprintf(w, "Creator: Organization: %s\n", textify(s))
+		fmt.Fprintf(w, "Creator: Organization: %s\n", s)
 	}
 	for _, s := range ci.CreatorTools {
-		fmt.Fprintf(w, "Creator: Tool: %s\n", textify(s))
+		fmt.Fprintf(w, "Creator: Tool: %s\n", s)
 	}
 	if ci.Created != "" {
-		fmt.Fprintf(w, "Created: %s\n", textify(ci.Created))
+		fmt.Fprintf(w, "Created: %s\n", ci.Created)
 	}
 	if ci.CreatorComment != "" {
 		fmt.Fprintf(w, "CreatorComment: %s\n", textify(ci.CreatorComment))
