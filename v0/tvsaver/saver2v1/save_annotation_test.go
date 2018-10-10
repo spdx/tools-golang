4c88fbd49f847482c9450e23375bad19c1a92627
Steve Winslow <swinslow@gmail.com>
2018-10-10 18:28:41 -0400

    Added tests and saving for Annotation section
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

---
+++ v0/tvsaver/saver2v1/save_annotation_test.go
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+
+package saver2v1
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/swinslow/spdx-go/v0/spdx"
+)
+
+// ===== Annotation section Saver tests =====
+func TestSaver2_1AnnotationSavesTextForPerson(t *testing.T) {
+	ann := &spdx.Annotation2_1{
+		Annotator:                "John Doe",
+		AnnotatorType:            "Person",
+		AnnotationDate:           "2018-10-10T17:52:00Z",
+		AnnotationType:           "REVIEW",
+		AnnotationSPDXIdentifier: "SPDXRef-DOCUMENT",
+		AnnotationComment:        "This is an annotation about the SPDX document",
+	}
+
+	// what we want to get, as a buffer of bytes
+	// no trailing blank newline
+	want := bytes.NewBufferString(`Annotator: Person: John Doe
+AnnotationDate: 2018-10-10T17:52:00Z
+AnnotationType: REVIEW
+SPDXREF: SPDXRef-DOCUMENT
+AnnotationComment: This is an annotation about the SPDX document
+`)
+
+	// render as buffer of bytes
+	var got bytes.Buffer
+	err := renderAnnotation2_1(ann, &got)
+	if err != nil {
+		t.Errorf("Expected nil error, got %v", err)
+	}
+
+	// check that they match
+	c := bytes.Compare(want.Bytes(), got.Bytes())
+	if c != 0 {
+		t.Errorf("Expected %v, got %v", want.String(), got.String())
+	}
+}
+
+func TestSaver2_1AnnotationSavesTextForOrganization(t *testing.T) {
+	ann := &spdx.Annotation2_1{
+		Annotator:                "John Doe, Inc.",
+		AnnotatorType:            "Organization",
+		AnnotationDate:           "2018-10-10T17:52:00Z",
+		AnnotationType:           "REVIEW",
+		AnnotationSPDXIdentifier: "SPDXRef-DOCUMENT",
+		AnnotationComment:        "This is an annotation about the SPDX document",
+	}
+
+	// what we want to get, as a buffer of bytes
+	// no trailing blank newline
+	want := bytes.NewBufferString(`Annotator: Organization: John Doe, Inc.
+AnnotationDate: 2018-10-10T17:52:00Z
+AnnotationType: REVIEW
+SPDXREF: SPDXRef-DOCUMENT
+AnnotationComment: This is an annotation about the SPDX document
+`)
+
+	// render as buffer of bytes
+	var got bytes.Buffer
+	err := renderAnnotation2_1(ann, &got)
+	if err != nil {
+		t.Errorf("Expected nil error, got %v", err)
+	}
+
+	// check that they match
+	c := bytes.Compare(want.Bytes(), got.Bytes())
+	if c != 0 {
+		t.Errorf("Expected %v, got %v", want.String(), got.String())
+	}
+}
+
+func TestSaver2_1AnnotationSavesTextForTool(t *testing.T) {
+	ann := &spdx.Annotation2_1{
+		Annotator:                "magictool-1.1",
+		AnnotatorType:            "Tool",
+		AnnotationDate:           "2018-10-10T17:52:00Z",
+		AnnotationType:           "REVIEW",
+		AnnotationSPDXIdentifier: "SPDXRef-DOCUMENT",
+		AnnotationComment:        "This is an annotation about the SPDX document",
+	}
+
+	// what we want to get, as a buffer of bytes
+	// no trailing blank newline
+	want := bytes.NewBufferString(`Annotator: Tool: magictool-1.1
+AnnotationDate: 2018-10-10T17:52:00Z
+AnnotationType: REVIEW
+SPDXREF: SPDXRef-DOCUMENT
+AnnotationComment: This is an annotation about the SPDX document
+`)
+
+	// render as buffer of bytes
+	var got bytes.Buffer
+	err := renderAnnotation2_1(ann, &got)
+	if err != nil {
+		t.Errorf("Expected nil error, got %v", err)
+	}
+
+	// check that they match
+	c := bytes.Compare(want.Bytes(), got.Bytes())
+	if c != 0 {
+		t.Errorf("Expected %v, got %v", want.String(), got.String())
+	}
+}
+
+// note that the annotation has no optional or multiple fields
