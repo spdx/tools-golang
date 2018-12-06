fe644adc096ddbc14cd4b797aea3a5890a3d0a02
Steve Winslow <swinslow@gmail.com>
2018-12-06 11:51:19 +0900

    Added implementation for license diff results set
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

--- v0/licensediff/licensediff.go
+++ v0/licensediff/licensediff.go
@@ -10,8 +10,8 @@ import (
 // LicensePair is a result set where we are talking about two license strings,
 // potentially differing, for a single filename between two SPDX Packages.
 type LicensePair struct {
-	first  string
-	second string
+	First  string
+	Second string
 }
 
 // MakePairs essentially just consolidates all files and LicenseConcluded
@@ -21,7 +21,7 @@ func MakePairs(p1 *spdx.Package2_1, p2 *spdx.Package2_1) (map[string]LicensePair
 
 	// first, go through and add all files/licenses from p1
 	for _, f := range p1.Files {
-		pair := LicensePair{first: f.LicenseConcluded, second: ""}
+		pair := LicensePair{First: f.LicenseConcluded, Second: ""}
 		pairs[f.FileName] = pair
 	}
 
@@ -32,12 +32,50 @@ func MakePairs(p1 *spdx.Package2_1, p2 *spdx.Package2_1) (map[string]LicensePair
 		existingPair, ok := pairs[f.FileName]
 		if ok {
 			// already present; update it
-			firstLic = existingPair.first
+			firstLic = existingPair.First
 		}
 		// now, update what's there, either way
-		pair := LicensePair{first: firstLic, second: f.LicenseConcluded}
+		pair := LicensePair{First: firstLic, Second: f.LicenseConcluded}
 		pairs[f.FileName] = pair
 	}
 
 	return pairs, nil
+}
+
+// LicenseDiff is a structured version of the output of MakePairs. It is
+// meant to make it easier to find and report on, e.g., just the files that
+// have different licenses, or those that are in just one scan.
+type LicenseDiff struct {
+	InBothChanged map[string]LicensePair
+	InBothSame    map[string]string
+	InFirstOnly   map[string]string
+	InSecondOnly  map[string]string
+}
+
+// MakeResults creates a more structured set of results from the output
+// of MakePairs.
+func MakeResults(pairs map[string]LicensePair) (*LicenseDiff, error) {
+	diff := &LicenseDiff{
+		InBothChanged: map[string]LicensePair{},
+		InBothSame:    map[string]string{},
+		InFirstOnly:   map[string]string{},
+		InSecondOnly:  map[string]string{},
+	}
+
+	// walk through pairs and allocate them where they belong
+	for filename, pair := range pairs {
+		if pair.First == pair.Second {
+			diff.InBothSame[filename] = pair.First
+		} else {
+			if pair.First == "" {
+				diff.InSecondOnly[filename] = pair.Second
+			} else if pair.Second == "" {
+				diff.InFirstOnly[filename] = pair.First
+			} else {
+				diff.InBothChanged[filename] = pair
+			}
+		}
+	}
+
+	return diff, nil
 }
