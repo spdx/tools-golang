fd35655d026007c437fdc54ef30ad6c963ba389d
Steve Winslow <steve@swinslow.net>
2019-12-08 18:38:30 -0500

    Fix tvloader package loading after unpackaged files
    Signed-off-by: Steve Winslow <steve@swinslow.net>

--- v0/tvloader/parser2v1/parse_package.go
+++ v0/tvloader/parser2v1/parse_package.go
@@ -19,7 +19,7 @@ func (parser *tvParser2_1) parsePairFromPackage2_1(tag string, value string) err
 	switch tag {
 	case "PackageName":
 		// if package already has a name, create and go on to a new package
-		if parser.pkg.PackageName != "" {
+		if parser.pkg.PackageName != "" || parser.pkg.IsUnpackaged == true {
 			parser.pkg = &spdx.Package2_1{
 				IsUnpackaged:              false,
 				FilesAnalyzed:             true,
