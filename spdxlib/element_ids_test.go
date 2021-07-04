9b311ccdd63ede93730728a72aa147e72898807e
Steve Winslow <steve@swinslow.net>
2021-07-04 13:01:58 -0400

    Add missing SPDX license IDs and go fmt
    Signed-off-by: Steve Winslow <steve@swinslow.net>

--- spdxlib/element_ids_test.go
+++ spdxlib/element_ids_test.go
@@ -1,16 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+
 package spdxlib
 
 import (
-    "github.com/spdx/tools-golang/spdx"
-    "reflect"
-    "testing"
+	"github.com/spdx/tools-golang/spdx"
+	"reflect"
+	"testing"
 )
 
 func TestSortElementIDs(t *testing.T) {
-    eIDs := []spdx.ElementID{"def", "abc", "123"}
-    eIDs = SortElementIDs(eIDs)
+	eIDs := []spdx.ElementID{"def", "abc", "123"}
+	eIDs = SortElementIDs(eIDs)
 
-    if !reflect.DeepEqual(eIDs, []spdx.ElementID{"123", "abc", "def"}) {
-        t.Fatalf("expected sorted ElementIDs, got: %v", eIDs)
-    }
+	if !reflect.DeepEqual(eIDs, []spdx.ElementID{"123", "abc", "def"}) {
+		t.Fatalf("expected sorted ElementIDs, got: %v", eIDs)
+	}
 }
