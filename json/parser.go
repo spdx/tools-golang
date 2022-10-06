6fda8118533aec0a73ca431cb32c7ca951b58200
Keith Zantow <kzantow@gmail.com>
2022-10-06 19:34:47 -0400

    chore: Add v2.3 data model and JSON support
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- json/parser.go
+++ json/parser.go
@@ -8,6 +8,7 @@ import (
 	"io"
 
 	"github.com/spdx/tools-golang/spdx/v2_2"
+	"github.com/spdx/tools-golang/spdx/v2_3"
 )
 
 // Load2_2 takes in an io.Reader and returns an SPDX document.
@@ -20,6 +21,24 @@ func Load2_2(content io.Reader) (*v2_2.Document, error) {
 	}
 
 	var doc v2_2.Document
+	err = json.Unmarshal(buf.Bytes(), &doc)
+	if err != nil {
+		return nil, err
+	}
+
+	return &doc, nil
+}
+
+// Load2_3 takes in an io.Reader and returns an SPDX document.
+func Load2_3(content io.Reader) (*v2_3.Document, error) {
+	// convert io.Reader to a slice of bytes and call the parser
+	buf := new(bytes.Buffer)
+	_, err := buf.ReadFrom(content)
+	if err != nil {
+		return nil, err
+	}
+
+	var doc v2_3.Document
 	err = json.Unmarshal(buf.Bytes(), &doc)
 	if err != nil {
 		return nil, err
--- json/writer.go
+++ json/writer.go
@@ -4,6 +4,7 @@ package spdx_json
 
 import (
 	"encoding/json"
+	"github.com/spdx/tools-golang/spdx/v2_3"
 	"io"
 
 	"github.com/spdx/tools-golang/spdx/v2_2"
@@ -11,6 +12,21 @@ import (
 
 // Save2_2 takes an SPDX Document (version 2.2) and an io.Writer, and writes the document to the writer in JSON format.
 func Save2_2(doc *v2_2.Document, w io.Writer) error {
+	buf, err := json.Marshal(doc)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(buf)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Save2_3 takes an SPDX Document (version 2.2) and an io.Writer, and writes the document to the writer in JSON format.
+func Save2_3(doc *v2_3.Document, w io.Writer) error {
 	buf, err := json.Marshal(doc)
 	if err != nil {
 		return err
