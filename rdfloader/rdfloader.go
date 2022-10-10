0b2bd4be354bdd719c14a720af0ee2140b676dcb
Keith Zantow <kzantow@gmail.com>
2022-10-10 12:36:03 -0400

    chore: support for 2.3 features to rdfloader
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- rdfloader/rdfloader.go
+++ rdfloader/rdfloader.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/spdx/gordf/rdfloader"
 	"github.com/spdx/tools-golang/rdfloader/parser2v2"
+	"github.com/spdx/tools-golang/rdfloader/parser2v3"
 	"github.com/spdx/tools-golang/spdx/v2_2"
+	"github.com/spdx/tools-golang/spdx/v2_3"
 )
 
 // Takes in a file Reader and returns the pertaining spdx document
@@ -19,5 +21,17 @@ func Load2_2(content io.Reader) (*v2_2.Document, error) {
 	}
 
 	doc, err := parser2v2.LoadFromGoRDFParser(rdfParserObj)
+	return doc, err
+}
+
+// Takes in a file Reader and returns the pertaining spdx document
+// or the error if any is encountered while setting the doc.
+func Load2_3(content io.Reader) (*v2_3.Document, error) {
+	var rdfParserObj, err = rdfloader.LoadFromReaderObject(content)
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := parser2v3.LoadFromGoRDFParser(rdfParserObj)
 	return doc, err
 }
